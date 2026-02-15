a6f25671e4ce16c7a65fd6022e761da6ca72ed44
jaskaurwalia91-blip <jaskaurwalia91@gmail.com>
2026-02-15 17:27:24 +0530

    Update app.py

--- construction-cms-newversion/app.py
+++ construction-cms-newversion/app.py
@@ -19,9 +19,10 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
 app.config['ALLOWED_EXTENSIONS'] = {'pdf', 'png', 'jpg', 'jpeg', 'doc', 'docx', 'xls', 'xlsx'}
-
 # Database Configuration (NeonDB)
-DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://neondb_owner:npg_C2pgKZH4sSJk@ep-autumn-river-aik6owco-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'')
+DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://neondb_owner:npg_C2pgKZH4sSJk@ep-autumn-river-aik6owco.us-east-1.aws.neon.tech/neondb?sslmode=require')
+# Database Configuration (NeonDB)
+#DATABASE_URL = os.environ.get('DATABASE_URL', 'postgresql://neondb_owner:npg_C2pgKZH4sSJk@ep-autumn-river-aik6owco-pooler.c-4.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'')
 
 def get_db():
     """Get database connection"""
@@ -757,5 +758,6 @@ if __name__ == '__main__':
     init_db()
 
     app.run(debug=True, host='0.0.0.0', port=5000)
+
 
 
